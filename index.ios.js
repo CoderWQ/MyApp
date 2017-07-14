e29ecadc119852b64e684143057f9e6ce44b88a0
coderXu <280960187@qq.com>
2017-07-14 17:59:10 +0800

    首次提交
    React Native版本的bs

---
+++ index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class MyApp extends Component {
+  render() {
+     let pic = {
+      uri: 'http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg'
+    };
+    return (
+      <Image source={pic} style={{top:200, width: 193, height: 110}} />
+    );
+     
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('MyApp', () => MyApp);
